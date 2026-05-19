6f186bfb9f06841cb0c0304acf38abef957190b0
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2026-05-19 14:06:46 -0700

    feat: update apex pwv through present

--- query_apex.py
+++ query_apex.py
@@ -11,7 +11,7 @@ end_date = dt.datetime(2026,5,18)
 
 request = requests.post(APEX_DATA_URL, data={
         'wdbo': 'csv/download',
-        'max_rows_returned': 300000,
+        'max_rows_returned': 900000,
         'start_date': start_date.strftime('%Y-%m-%dT%H:%M:%S') + '..' \
             + end_date.strftime('%Y-%m-%dT%H:%M:%S'),
         'tab_pwv': 'on',
