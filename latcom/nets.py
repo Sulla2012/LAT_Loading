45cadb8ddf724362ac336e4652727be5d2b964c0
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2026-06-15 13:50:53 -0700

    feat: refactor net to be parallelized

--- latcom/nets.py
+++ latcom/nets.py
@@ -1,37 +1,20 @@
 import datetime as dt
 import glob
+import multiprocessing
+from functools import partial
 from zoneinfo import ZoneInfo
 
 import dill as pk
-import numpy as np
 from sotodlib import core
-from sotodlib.core.metadata.loader import LoaderError
-from sotodlib.tod_ops.flags import get_det_bias_flags
 
 import latcom.utils.net_utils as nu
 from latcom.utils.optical_loading import pwv_interp
 
-results = sorted(glob.glob("../results_*.pk"))[-1]
-abscals = sorted(glob.glob("../abscals_*.pk"))[-1]
+result_path = sorted(glob.glob("../results_*.pk"))[-1]
+abscal_path = sorted(glob.glob("../abscals_*.pk"))[-1]
 
-with open(results, "rb") as f:
+with open(result_path, "rb") as f:
     result_dict = pk.load(f)
-
-with open(abscals, "rb") as f:
-    abscal_dict = pk.load(f)
-
-try:
-    nets = sorted(glob.glob("../nets_*.pk"))[-1]
-    with open(nets, "rb") as f:
-        net_dict = pk.load(f)
-
-except (OSError, FileNotFoundError, IndexError):
-    today = dt.datetime.now(tz=ZoneInfo("America/New_York")).date()
-    date_str = str(today.month).zfill(2) + str(today.day).zfill(2) + str(today.year)
-    nets = f"../nets_{date_str}.pk"
-    net_dict = nu.gen_empty_net_dict(abscal_dict)
-
-# ctx = core.Context("../smurf_det_preproc.yaml")
 
 ctx = core.Context("../smurf_det_preproc.yaml")
 
@@ -43,124 +26,15 @@ obs_list = ctx.obsdb.query(
 
 pwv = pwv_interp()
 
-if "index" in net_dict:
-    start_index = net_dict["index"]
-else:
-    start_index = 0
+with multiprocessing.Pool() as pool:
+    driver_func = partial(nu.get_nets, abscal_list=result_dict.keys(), pwv=pwv)
+    results = pool.map(nu.get_nets, obs_list)
 
-no_preproc = []
+net_dict = nu.parse_net_results(results, abscal_path)
 
-for i in range(start_index, len(obs_list)):
-    cur_obs = obs_list[i]
-
-    if i % 100 == 0:
-        net_dict["index"] = i
-        print("Index: ", net_dict["index"])
-        with open(nets, "wb") as f:
-            pk.dump(net_dict, f)
-
-    try:  # Much faster than ctx.get_meta
-        det_info = ctx.get_det_info(cur_obs["obs_id"])
-    except LoaderError:
-        print("No meta data for obs {}".format(cur_obs["obs_id"]))
-        continue
-    wafers = np.unique(det_info["stream_id"])
-    bands = np.unique(det_info["wafer.bandpass"])
-    bands = np.array([b[1:] for b in bands if len(b) > 1 and b[0] == "f"])
-
-    for j in range(len(wafers)):
-        cur_wafer = wafers[j].split("_")[-1]
-        if "mv" in cur_wafer:
-            if (
-                cur_obs["obs_id"] in net_dict[cur_wafer]["090"]["obs"]
-                and cur_obs["obs_id"] in net_dict[cur_wafer]["150"]["obs"]
-            ):
-                print("Already done")
-                continue
-        elif "uv" in cur_wafer and (
-            cur_obs["obs_id"] in net_dict[cur_wafer]["220"]["obs"]
-            and cur_obs["obs_id"] in net_dict[cur_wafer]["280"]["obs"]
-        ):
-            print("Already done")
-            continue
-
-        if cur_wafer not in result_dict:
-            print(f"No abscal for ufm {cur_wafer}")
-            continue
-
-        for band in bands:
-            if "mv" in cur_wafer:
-                if band == "090":
-                    ufm_band = 1
-                elif band == "150":
-                    ufm_band = 2
-            if "uv" in cur_wafer:
-                if band == "220":
-                    ufm_band = 1
-                elif band == "280":
-                    ufm_band = 2
-
-            try:
-                meta = ctx.get_meta(
-                    cur_obs["obs_id"],
-                    dets={
-                        "dets:stream_id": "ufm_" + str(cur_wafer),
-                        "dets:wafer.bandpass": "f" + str(band),
-                    },
-                )
-            except LoaderError:
-                print("No meta data for obs {}".format(cur_obs["obs_id"]))
-                no_preproc.append(cur_obs["obs_id"])
-                continue
-            flags = get_det_bias_flags(meta).det_bias_flags
-            meta.restrict("dets", ~core.flagman.has_any_cuts(flags))
-            wafer_flag = np.array([cur_wafer in ufm for ufm in meta.det_info.stream_id])
-
-            if len(wafer_flag) == 0:
-                print("No det_info for obs {}".format(cur_obs["obs_id"]))
-                continue
-
-            bp = (meta.det_cal.bg % 4) // 2
-
-            if ufm_band == 1:
-                net_flag = wafer_flag * (bp == 0)
-            elif ufm_band == 2:
-                net_flag = wafer_flag * (bp == 1)
-
-            raw_cal = np.nanmedian(meta.abscal.raw_abscal_rj[net_flag])
-            if "noise" in meta.preprocess:
-                wnoise = meta.preprocess.noise.white_noise[net_flag]
-            elif "noiseT" in meta.preprocess:
-                wnoise = meta.preprocess.noiseT.white_noise[net_flag]
-            else:
-                print(f"Error: no valid noise ken in {meta.preprocess.keys()}")
-                continue
-            ndets = len(np.where(wnoise != 0)[0])
-
-            net_mes = 1 / np.sqrt(2) * wnoise * raw_cal
-            clean_nets = []
-            for net in net_mes:
-                if net * 1e6 > 100:
-                    clean_nets.append(net)
-            clean_nets = np.array(clean_nets)
-            array_net = np.nansum((clean_nets * 1e6) ** (-2)) ** (-1 / 2)
-
-            net_dict[cur_wafer][band]["raw_cal"].append(raw_cal)
-            net_dict[cur_wafer][band]["obs"].append(cur_obs["obs_id"])
-            net_dict[cur_wafer][band]["ndets"].append(ndets)
-            net_dict[cur_wafer][band]["nets"].append(array_net)
-            net_dict[cur_wafer][band]["pwv"].append(pwv(cur_obs["timestamp"]))
-            net_dict[cur_wafer][band]["el"].append(meta.obs_info.el_center)
-            net_dict[cur_wafer][band]["neps"].append(wnoise)
-            net_dict[cur_wafer][band]["phiconv"].append(
-                meta.det_cal.phase_to_pW[net_flag]
-            )
-
-
-print("Final Dump")
+today = dt.datetime.now(tz=ZoneInfo("America/New_York")).date()
+date_str = str(today.month).zfill(2) + str(today.day).zfill(2) + str(today.year)
+nets = f"../nets_{date_str}.pk"
 
 with open(nets, "wb") as f:
     pk.dump(net_dict, f)
-
-with open("no_preproc.txt", "w") as f:
-    f.write("\n".join(no_preproc))
--- latcom/utils/net_utils.py
+++ latcom/utils/net_utils.py
@@ -1,3 +1,13 @@
+from collections import OrderedDict
+
+import dill as pk
+import numpy as np
+from scipy import interpolate
+from sotodlib import core
+from sotodlib.core.metadata.loader import LoaderError
+from sotodlib.tod_ops.flags import get_det_bias_flags
+
+
 def gen_empty_net_dict(abscal_dict: dict) -> dict:
     """
     Generate an emtpy net_dict with the appropriate keys from an abscal dict.
@@ -87,5 +97,219 @@ def gen_empty_net_dict(abscal_dict: dict) -> dict:
                     "phiconv": [],
                 },
             }
+
+    return net_dict
+
+
+def get_nets(
+    obs: OrderedDict,
+    abscal_list: list,
+    pwv: interpolate.interp1d,
+    ctx: core.context.Context,
+) -> tuple[list, list, list, list, list, list, list, list, list, list] | None:
+    """
+    Function which computes the NET as well as NEP and some other parameters
+    for a given observation. Computes these parameters over all available
+    wafers and bands. Returns none if there is no metadata for the obs.
+
+    Parameters
+    ----------
+    obs : OrderedDict
+        Observation dict as returned by ctx.obsdb.query
+    abscal_list : list
+        List of array/band combinations we have abscals for.
+    pwv : interpolate-interp1d
+        Interpolation function for pwv.
+    ctx : core.context.Context
+        Context object for loading metadata
+
+    Returns
+    -------
+    arrays : list
+        List of arrays corresponding to each return entry
+    bands : list
+        List of bands corresponding to each return entry
+    raw_cals : list
+        Raw abscal for array, band at time of obs
+    obs_ids : list
+        Obs ids corresponding to each return entry
+    ndets : list
+        Number of functioning detectors
+    array_nets : list
+        Inverse variance averaged NET over the array
+    pwvs : list
+        PWVs corresponding to each return entry
+    els : list
+        Central elevations corresponding to each return entry
+    neps : list[list]
+        List of per detector NEPs
+    phiconvs : lsit
+        Phi to pW conversions corresponding to each return entry
+
+    """
+
+    arrays = []
+    bands = []
+
+    raw_cals = []
+    obs_ids = []
+    ndets = []
+    array_nets = []
+    pwvs = []
+    els = []
+    neps = []
+    phiconvs = []
+    try:  # Much faster than ctx.get_meta
+        det_info = ctx.get_det_info(obs["obs_id"])
+    except LoaderError:
+        print("No meta data for obs {}".format(obs["obs_id"]))
+        return None
+    wafers = np.unique(det_info["stream_id"])
+    bands = np.unique(det_info["wafer.bandpass"])
+    bands = np.array([b[1:] for b in bands if len(b) > 1 and b[0] == "f"])
+
+    for j in range(len(wafers)):
+        cur_wafer = wafers[j].split("_")[-1]
+
+        if cur_wafer not in abscal_list:
+            print(f"No abscal for ufm {cur_wafer}")
+            continue
+
+        for band in bands:
+            if "mv" in cur_wafer:
+                if band == "090":
+                    ufm_band = 1
+                elif band == "150":
+                    ufm_band = 2
+            if "uv" in cur_wafer:
+                if band == "220":
+                    ufm_band = 1
+                elif band == "280":
+                    ufm_band = 2
+
+            try:
+                meta = ctx.get_meta(
+                    obs["obs_id"],
+                    dets={
+                        "dets:stream_id": "ufm_" + str(cur_wafer),
+                        "dets:wafer.bandpass": "f" + str(band),
+                    },
+                )
+            except LoaderError:
+                print("No meta data for obs {}".format(obs["obs_id"]))
+                continue
+
+            flags = get_det_bias_flags(meta).det_bias_flags
+            meta.restrict("dets", ~core.flagman.has_any_cuts(flags))
+            wafer_flag = np.array([cur_wafer in ufm for ufm in meta.det_info.stream_id])
+
+            if len(wafer_flag) == 0:
+                print("No det_info for obs {}".format(obs["obs_id"]))
+                continue
+
+            bp = (meta.det_cal.bg % 4) // 2
+
+            if ufm_band == 1:
+                net_flag = wafer_flag * (bp == 0)
+            elif ufm_band == 2:
+                net_flag = wafer_flag * (bp == 1)
+
+            raw_cal = np.nanmedian(meta.abscal.raw_abscal_rj[net_flag])
+            if "noise" in meta.preprocess:
+                wnoise = meta.preprocess.noise.white_noise[net_flag]
+            elif "noiseT" in meta.preprocess:
+                wnoise = meta.preprocess.noiseT.white_noise[net_flag]
+            else:
+                print(f"Error: no valid noise ken in {meta.preprocess.keys()}")
+                continue
+            ndet = len(np.where(wnoise != 0)[0])
+
+            net_mes = 1 / np.sqrt(2) * wnoise * raw_cal
+            clean_nets = []
+            for net in net_mes:
+                if net * 1e6 > 100:
+                    clean_nets.append(net)
+            clean_nets = np.array(clean_nets)
+            array_net = np.nansum((clean_nets * 1e6) ** (-2)) ** (-1 / 2)
+
+            arrays.append(cur_wafer)
+            bands.append(band)
+
+            raw_cals.append(raw_cal)
+            obs_ids.append(obs["obs_id"])
+            ndets.append(ndet)
+            array_nets.append(array_net)
+            pwvs.append(pwv(obs["timestamp"]))
+            els.append(meta.obs_info.el_center)
+            neps.append(wnoise)
+            phiconvs.append(meta.det_cal.phase_to_pW[net_flag])
+
+    return (
+        arrays,
+        bands,
+        raw_cals,
+        obs_ids,
+        ndets,
+        array_nets,
+        pwvs,
+        els,
+        neps,
+        phiconvs,
+    )
+
+
+def parse_net_results(results: list, abscal_path: str) -> tuple[dict, str]:
+    """
+    Function which parses the list based results of get_nets
+    into the expected results dictionary. get_nets is set to
+    be parallelizable, so it has to return list instead of the
+    desired dict.
+
+    Parameters
+    ----------
+    results : list
+        List of results by obs as produced by get_nets
+    abscal_path :
+        Path to the abscal dict.
+
+    Returns
+    -------
+    net_dict : dict
+        Result dictionary of results reorganized by array/band
+
+    """
+
+    with open(abscal_path, "rb") as f:
+        abscal_dict = pk.load(f)
+
+    net_dict = gen_empty_net_dict(abscal_dict)
+
+    for result in results:
+        if result is None:
+            continue
+        (
+            arrays,
+            bands,
+            raw_cals,
+            obs_ids,
+            ndets,
+            array_nets,
+            pwvs,
+            els,
+            neps,
+            phiconvs,
+        ) = result
+        for i in len(arrays):
+            array = arrays[i]
+            band = bands[i]
+            net_dict[array][band]["raw_cal"].append(raw_cals[i])
+
+            net_dict[array][band]["obs"].append(obs_ids[i])
+            net_dict[array][band]["ndets"].append(ndets[i])
+            net_dict[array][band]["nets"].append(array_nets[i])
+            net_dict[array][band]["pwv"].append(pwvs[i])
+            net_dict[array][band]["el"].append(els[i])
+            net_dict[array][band]["neps"].append(neps[i])
+            net_dict[array][band]["phiconv"].append(phiconvs[i])
 
     return net_dict
