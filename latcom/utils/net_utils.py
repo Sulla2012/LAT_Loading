fix: bug with parse_dict and with net units

latcom/utils/net_utils.py
@@ -240,12 +240,17 @@ def get_nets(
             continue
 
         for band in bands:
-            if "mv" in cur_wafer:
+            if "ln" in cur_wafer:
+                if band == "030":
+                    ufm_band = 1
+                elif band == "040":
+                    ufm_band = 2
+            elif "mv" in cur_wafer:
                 if band == "090":
                     ufm_band = 1
                 elif band == "150":
                     ufm_band = 2
-            if "uv" in cur_wafer:
+            elif "uv" in cur_wafer:
                 if band == "220":
                     ufm_band = 1
                 elif band == "280":
@@ -288,10 +293,22 @@ def get_nets(
                 continue
             ndet = len(np.where(wnoise != 0)[0])
 
-            net_mes = 1 / np.sqrt(2) * wnoise * raw_cal
+            # Shitty units check
+            try:
+                if 1e-4 < wnoise[wnoise > 0][0] < 1e-3:
+                    net_mes = wnoise
+                    factor = 1e6
+                elif 1e-1 < wnoise[wnoise > 0][0]:
+                    net_mes = 1 / np.sqrt(2) * wnoise * raw_cal
+                    factor = 1
+                else:
+                    net_mes = 1 / np.sqrt(2) * wnoise * raw_cal
+                    factor = 1e6
+            except IndexError:
+                continue
             clean_nets = []
             for net in net_mes:
-                if net * 1e6 > 100:
+                if net * factor > 100:
                     clean_nets.append(net)
             clean_nets = np.array(clean_nets)
             array_net = np.nansum((clean_nets * 1e6) ** (-2)) ** (-1 / 2)

latcom/utils/parsing.py
@@ -33,7 +33,13 @@ def parse_dict(net_dict: dict) -> pd.DataFrame:
     for freq in (
         freqs
     ):  # This is slighly inefficient but the ezest way to sort by freq then ufm
-        if freq in ["090", "150"]:
+        if freq in ["030", "040"]:
+            flavor = "LF"
+            if freq == "030":
+                ch = "LF_1"
+            else:
+                ch = "LF_2"
+        elif freq in ["090", "150"]:
             flavor = "MF"
             if freq == "090":
                 ch = "MF_1"
@@ -135,13 +141,14 @@ def parse_neps(net_dict: dict) -> pd.DataFrame:
     pwv = pwv_interp()
     nep_labels = []
     neps = []
-    cals = []
     neis = []
     obs = []
     pwvs = []
     els = []
     pwv_sinel = []
     abscals = []
+    yields = []
+    t_obs = []
 
     ufms = sorted(net_dict.keys())[1:]  # remove index key
 
@@ -157,40 +164,35 @@ def parse_neps(net_dict: dict) -> pd.DataFrame:
                 for sub_key in sub_dict:
                     if freq not in sub_key:
                         continue
-                    cur_abscals = np.array(sub_dict[sub_key]["raw_cal"])
-                    cur_nets = np.array(sub_dict[sub_key]["nets"])
                     cur_neps = sub_dict[sub_key]["neps"]
                     cur_obs = np.array(sub_dict[sub_key]["obs"])
                     cur_ndets = np.array(sub_dict[sub_key]["ndets"])
-                    cur_phicals = sub_dict[sub_key]["phiconv"]
                     cur_el = np.array(sub_dict[sub_key]["el"])
+                    cur_phicals = sub_dict[sub_key]["phiconv"]
                     label = str(freq) + "_" + str(ufm)
-                    for j in range(len(cur_abscals)):
-                        cur_pwv = pwv(cur_obs[j].split("_")[1])
-                        if (
-                            cur_nets[j] <= 100 and cur_ndets[j] > 100 and cur_pwv < 6
-                        ):  # very large nets are not real
-                            for i, nep in enumerate(cur_neps[j]):
-                                abscals.append(cur_abscals[j])
-                                neps.append(nep)
-                                nep_labels.append(label)
-                                cals.append(cur_phicals[j][i])
-                                neis.append(nep / cur_phicals[j][i] * 9e6 / (2 * np.pi))
-                                obs.append(cur_obs[j])
-                                pwvs.append(cur_pwv)
-                                els.append(cur_el[j])
-                                pwv_sinel.append(
-                                    cur_pwv / np.sin(np.deg2rad(cur_el[j]))
-                                )
+                    for j in range(len(cur_obs)):
+                        cur_time = cur_obs[j].split("_")[1]
+                        cur_pwv = pwv(cur_time)
+
+                        for i, nep in enumerate(cur_neps[j]):
+                            neps.append(nep)
+                            nep_labels.append(label)
+                            neis.append(nep / cur_phicals[j][i] * 9e6 / (2 * np.pi))
+                            obs.append(cur_obs[j])
+                            pwvs.append(cur_pwv)
+                            els.append(cur_el[j])
+                            yields.append(cur_ndets[j] / 860)
+                            t_obs.append(float(cur_time))
+                            pwv_sinel.append(cur_pwv / np.sin(np.deg2rad(cur_el[j])))
     nep_labels = np.array(nep_labels)
     neps = np.array(neps)
     neis = np.array(neis)
     obs = np.array(obs)
-    cals = np.array(cals)
     pwvs = np.array(pwvs)
     els = np.array(els)
     pwv_sinel = np.array(pwv_sinel)
-    abscals = np.array(abscals)
+    yields = np.array(yields)
+    t_obs = np.array(t_obs)
 
     nep_df = pd.DataFrame(
         {
@@ -200,9 +202,9 @@ def parse_neps(net_dict: dict) -> pd.DataFrame:
             "neps": neps,
             "neis": neis,
             "el": els,
-            "cals": cals,
             "pwv_sinel": pwv_sinel,
-            "abscals": abscals,
+            "yields": yields,
+            "t_obs": t_obs,
         }
     )
     return nep_df